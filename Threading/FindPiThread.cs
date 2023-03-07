655f7c71da2c62c3c743fc8c5b6fac801dbed4d0
Spencer Karpati <spebkarp@ut.utm.edu>
2023-03-06 22:34:24 -0600

    Add project files.

---
+++ Threading/FindPiThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class FindPiThread
+    {
+        int dartNum = 0;
+        int dartboardCount;
+        public int dartBoard
+        {
+            get { return dartboardCount; }
+            set { dartboardCount = value; }
+        }
+        Random dartThrow;
+        
+        public FindPiThread(int dartNum)
+        {
+            this.dartNum = dartNum;
+            dartboardCount = 0;
+            dartThrow = new Random();
+        }
+
+        public void throwDarts()
+        {
+            for(int i = 0; i <= dartNum; i++)
+            {
+                var x = dartThrow.NextDouble();
+                var y = dartThrow.NextDouble(); 
+                
+                if((x * x) + (y * y) < 1)
+                {
+                    dartboardCount++;
+                }
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many throws should each thread make?: ");
+            string userThrow = Console.ReadLine();
+
+            Console.WriteLine("How many threads should run?: ");
+            string userThread = Console.ReadLine();
+
+            List <Thread> threads = new List <Thread>(Int32.Parse(userThread));
+            List <FindPiThread> list = new List <FindPiThread>(Int32.Parse(userThread));
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                FindPiThread findPi = new FindPiThread(Int32.Parse(userThrow));
+                list.Add(findPi);
+                Thread thread = new Thread(new ThreadStart(findPi.throwDarts));
+                thread.Start();
+                Thread.Sleep(16);
+            }
+
+            for(int i = 0; i < threads.Count; i++) 
+            {
+                threads[i].Join();
+            }
+
+            for(int i = 0; i < list.Count; i++) 
+            {
+                FindPiThread findPi = list[i];;
+                Console.WriteLine(4 * (findPi.dartBoard / Int32.Parse(userThrow)));
+            }
+            Console.ReadLine();
+        }
+    }
+}
